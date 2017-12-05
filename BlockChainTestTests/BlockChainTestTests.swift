af58b9ccd600ce36f66c6374748ab35220c3afa8
John Jones <john.jones@williamhill.com>
2017-12-05 17:25:28 +0100

    Initial Commit

---
+++ BlockChainTestTests/BlockChainTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  BlockChainTestTests.swift
+//  BlockChainTestTests
+//
+//  Created by John Jones on 05/12/2017.
+//  Copyright © 2017 William Hill. All rights reserved.
+//
+
+import XCTest
+@testable import BlockChainTest
+
+class BlockChainTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
